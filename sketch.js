44b5393c4f89840e3ee5e2996fbeb429629979dc
dongoift <do@infinitech.dev>
2021-12-24 09:57:10 +0700

    fix bg

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
 const Agent=function(){
-    this.vector=createVector(random(width),random(height)),
+    this.vector=createVector(random(width),-200),
     this.vectorOld=this.vector.copy(),
     this.stepSize=random(1,5),
     this.isOutside=!1,
     this.angle=0
 };
 Agent.prototype.update=function(t){
-    this.vector.x+=cos(this.angle+Math.PI*1.5)*this.stepSize,
-    this.vector.y+=sin(this.angle+Math.PI*1.5)*this.stepSize,
-    this.isOutside=this.vector.x<-20||this.vector.x>width+20||this.vector.y<-200||this.vector.y>height+200,
-    this.isOutside&&(this.vector.set(-20,random(height+200)-200),
+
+    this.vector.x+=sin(this.angle+Math.PI*1.5)*this.stepSize,
+    this.vector.y+=cos(this.angle+Math.PI*1.5)*this.stepSize
+
+    this.isOutside=this.vector.x<0||this.vector.x>width||this.vector.y<-200||this.vector.y>height+200,
+    this.isOutside&&(this.vector.set(-200,random(height+20)-20),
     this.vectorOld=this.vector.copy()),
     strokeWeight(t*this.stepSize),
     line(this.vectorOld.x,this.vectorOld.y,this.vector.x,this.vector.y),
@@ -18,24 +20,28 @@ Agent.prototype.update=function(t){
 },
 Agent.prototype.update1=function(t,e,i){
     this.angle=noise(this.vector.x/t,this.vector.y/t)*e,
-    this.update(i)};
-    let agents=[],
-      agentCount=0;
-    const noiseScale=500,
-      noiseStrength=3,
-      overlayAlpha=30,
-      agentAlpha=255,
-      strokeWidth=.3;
-    function setup(){
-      createCanvas(windowWidth ,windowHeight),
-      agentCount=windowHeight*.75;
-      for(let t=0;t<agentCount;t++)agents[t]=new Agent
-    }
-    function draw(){
-      fill(175,195,188,overlayAlpha),
-      noStroke(),
-      rect(0,0,width,height),
-      stroke(220,235,220,agentAlpha);
-      for(let t=0;t<agentCount;t++)agents[t].update1(noiseScale,noiseStrength,strokeWidth)
-    }
-    function windowResized(){resizeCanvas(windowWidth,windowHeight)}
+    this.update(i)
+};
+
+
+let agents=[],
+  agentCount=0;
+const noiseScale=500,
+  noiseStrength=3,
+  overlayAlpha=30,
+  agentAlpha=255,
+  strokeWidth=.2;
+
+function setup(){
+  createCanvas(windowWidth ,windowHeight),
+  agentCount=windowWidth*.2;
+  for(let t=0;t<agentCount;t++)agents[t]=new Agent
+}
+function draw(){
+  fill(255,255,255,.6),
+  noStroke(),
+  rect(0,0,width,height),
+  stroke(220,235,220,agentAlpha);
+  for(let t=0;t<agentCount;t++)agents[t].update1(noiseScale,noiseStrength,strokeWidth)
+}
+function windowResized(){resizeCanvas(windowWidth,windowHeight)}
