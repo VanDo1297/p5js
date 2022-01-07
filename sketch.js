228d5f007aa9f51aad46c0eea5017bfceed59ccb
dongoift <do@infinitech.dev>
2022-01-07 15:05:52 +0700

    update sketch

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const Agent=function(){
-    this.vector=createVector(random(-500,width + 500),-200),
+    this.vector=createVector(random(-width,width + 500),-200),
     this.vectorOld=this.vector.copy(),
     this.stepSize=random(1,5),
     this.isOutside=!1,
@@ -7,12 +7,16 @@ const Agent=function(){
 };
 Agent.prototype.update=function(t){
 
-    this.vector.x+=sin(this.angle+Math.PI*1.5)*this.stepSize,
+    this.vector.x+=sin(this.angle+Math.PI * 1.2)*this.stepSize,
     this.vector.y+=cos(this.angle+Math.PI*1.5)*this.stepSize
 
-    this.isOutside=this.vector.x<0||this.vector.x>width||this.vector.y<-200||this.vector.y>height+200,
-    this.isOutside&&(this.vector.set(random(-500,width + 500),-200),
-    this.vectorOld=this.vector.copy()),
+    this.isOutside=this.vector.x<0||this.vector.x>width||this.vector.y<-200||this.vector.y>height+200
+    if(this.isOutside){
+      this.vector.x = random(-500,width + 500);
+      this.vector.y = -200;
+      this.vectorOld=this.vector.copy()
+    }
+    
     strokeWeight(t*this.stepSize),
     line(this.vectorOld.x,this.vectorOld.y,this.vector.x,this.vector.y),
     this.vectorOld=this.vector.copy(),
@@ -34,13 +38,13 @@ const noiseScale=500,
 
 function setup(){
   createCanvas(windowWidth * 1.5 ,windowHeight),
-  agentCount=windowWidth*.5;
+  agentCount=windowWidth * 0.3;
   for(let t=0;t<agentCount;t++)agents[t]=new Agent
 }
 function draw(){
   fill(255,255,255,0),
   noStroke(),
-  rect(0,0,width * 1.5,height),
+  rect(0,0,width * 1.5,height * 1.5),
   stroke(220,235,220,agentAlpha);
   for(let t=0;t<agentCount;t++)agents[t].update1(noiseScale,noiseStrength,strokeWidth)
 }
